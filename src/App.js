a208d96829db132084ccb25aa8303c8587b65fd3
InviBull <virtualclass.raghav@gmail.com>
2021-10-16 08:54:24 +0530

    mostly functional

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { FaPlus, FaTrash, FaTimes} from 'react-icons/fa';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [notesData, setNotesData] = useState([]);
+  const newNoteInputBox = document.querySelector("#newNoteInputBox");
+
+  const newNoteButtonClicked = () => {
+    
+    const note = newNoteInputBox.value.toString();
+    
+    if (note) {
+      const notes = notesData;
+      notes.push({ note });
+
+      newNoteInputBox.value = "";
+
+      localStorage.setItem("notes", JSON.stringify(notes));
+      setNotesData(notesDat => [...notesDat]);
+
+    }
+  };
+
+  const clearNoteButtonClicked = () => {
+    
+    localStorage.clear();
+    setNotesData([]);
+
+  }
+
+  const deleteNoteButtonClicked = (key) => {
+    
+    const noteToDel = document.getElementById(`${key.key}`);
+    let pToDel = noteToDel.getElementsByTagName('p');
+
+    if (pToDel !== 'undefined' && pToDel !== 'null');
+    
+      pToDel = pToDel[0];
+      
+      let notes = notesData;
+      notes = notes.filter(noteObject => noteObject.note !== pToDel.textContent);
+
+      localStorage.setItem("notes", JSON.stringify(notes));
+      setNotesData(notesDat => [...notesDat]);
+  }
+
+  useEffect(() => {
+    let getNotes = localStorage.getItem("notes");
+  
+    if (getNotes !== '' && getNotes !== null){
+      return setNotesData(JSON.parse(getNotes));
+    }
+
+    return setNotesData([]);  
+  }, []);
+
+  const Notes = (noteProp) => {
+    return (
+      noteProp.data.map((noteObject, key) => {
+        return(
+          <div key={key} id = {key} className = "note-card">
+             <p>{noteObject.note}</p>
+             <button className = "deleteNoteClickButton" onClick={() => deleteNoteButtonClicked({key})}><FaTimes /></button>
+          </div>
+        )
+      })
+    )
+  };
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className = "container">
+        <div className = "newNoteInput">
+          <input type = "text" id="newNoteInputBox" placeholder = "Enter a new note..."/>
+          <button id = "newNoteClickButton" onClick={() => newNoteButtonClicked()}><FaPlus /></button>
+          <button id = "clearNoteClickButton" onClick={() => clearNoteButtonClicked()}><FaTrash /></button>
+        </div>
+        <div className = "notes">
+          <Notes data={notesData} />
+        </div>
+      </div>
+    </>
   );
 }
 
